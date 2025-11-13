5dfee3cf1e85227b36bd1101e10754a0f63bc2b4
Desh Iyer <desh.iyer@gmail.com>
2025-11-13 18:44:11 +0000

    Setup autocommands for formatting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -29,22 +29,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- format on save using efm langserver and configured formatters
--- local lsp_fmt_group = vim.api.nvim_create_augroup("FormatOnSaveGroup", {})
--- vim.api.nvim_create_autocmd("BufWritePre", {
---     group = lsp_fmt_group,
---     callback = function()
---         local efm = vim.lsp.get_clients({ name = "efm" })
---         if vim.tbl_isempty(efm) then
---             return
---         end
---         vim.lsp.buf.format({ name = "efm", async = true })
---     end,
--- })
---
--- -- on attach function shortcuts
--- local lsp_on_attach_group = vim.api.nvim_create_augroup("LspMappings", {})
--- vim.api.nvim_create_autocmd("LspAttach", {
---     group = lsp_on_attach_group,
---     callback = on_attach,
--- })
+-- Format on save using efm langserver and configured formatters
+local lsp_fmt_group = vim.api.nvim_create_augroup("FormatOnSaveGroup", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = lsp_fmt_group,
+    callback = function()
+        local efm = vim.lsp.get_clients({ name = "efm" })
+        if vim.tbl_isempty(efm) then
+            return
+        end
+        vim.lsp.buf.format({ name = "efm", async = true })
+    end,
+})
+
+-- on_attach function shortcuts
+local lsp_on_attach_group = vim.api.nvim_create_augroup("LspMappings", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = lsp_on_attach_group,
+    callback = on_attach,
+})
