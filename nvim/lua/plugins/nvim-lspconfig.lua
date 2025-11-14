8c15cd00a4c519a533977f8b1e5ebc354552aa1e
Desh Iyer <desh.iyer@gmail.com>
2025-11-14 13:40:42 +0000

    Remove init opts table for cmp and efmls

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,14 +11,13 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-        -- Mason is an LSP specifically for LSPs
+		-- Mason is an LSP specifically for LSPs
 		{ "mason-org/mason.nvim", opts = {} }, -- LSP/DAP/Linter installer & manager
-        "creativenull/efmls-configs-nvim", -- Preconfigured EFM Language Server setups
-        "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
+		"creativenull/efmls-configs-nvim", -- Preconfigured EFM Language Server setups
+		"hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
 	},
 	config = function()
 		require("utils.diagnostics").setup()
 		require("servers")
 	end,
-
 }
