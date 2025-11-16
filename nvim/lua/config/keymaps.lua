6b15a0a3b7e4e742c7f4d1632611b67da1726bff
Desh Iyer <desh.iyer@gmail.com>
2025-11-16 17:32:43 +0000

    Move keymaps into keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
--- TITLE: NeoVim keymaps
--- ABOUT: Sets some quality-of-life keymaps
+-- ================================================================================================
+-- TITLE : keymaps.lua
+-- ABOUT : Some quality-of-life keymaps (native and plugins)
+-- ================================================================================================
 
 -- Center screen when jumping
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
@@ -38,5 +40,40 @@ vim.keymap.set("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", { desc =
 -- File Explorer
 vim.keymap.set("n", "<leader>m", "<Cmd>NvimTreeFocus<CR>", { desc = "Focus on File Explorer" })
 vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
-vim.keymap.set('n', '<leader>t', ':belowright 10sp | term<CR>', { desc = "Toggle Terminal", noremap = true, silent = true })
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "ESC exit Terminal Insert Mode", noremap = true })
+vim.keymap.set(
+	"n",
+	"<leader>t",
+	":belowright 10sp | term<CR>",
+	{ desc = "Toggle Terminal", noremap = true, silent = true }
+)
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "ESC exit Terminal Insert Mode", noremap = true })
+
+local opts = {
+	noremap = true, -- prevent recursive mapping
+	silent = false, -- don't print the command to the cli
+	-- buffer = bufnr, -- restrict the vim.keymap.set to the local buffer number
+}
+
+-- File Diagnostics
+vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- goto definition
+vim.keymap.set("n", "<leader>gS", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts) -- goto definition in split
+vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Code actions
+vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts) -- Rename symbol
+vim.keymap.set("n", "<leader>D", "<cmd>lua vim.diagnostic.open_float({ scope = 'line' })<CR>", opts) -- Line diagnostics (float)
+vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts) -- Cursor diagnostics
+vim.keymap.set("n", "<leader>pd", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts) -- previous diagnostic
+vim.keymap.set("n", "<leader>nd", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts) -- next diagnostic
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) -- hover documentation
+
+-- fzf-lua keymaps
+vim.keymap.set("n", "<leader>gd", "<cmd>FzfLua lsp_finder<CR>", opts) -- LSP Finder (definition + references)
+vim.keymap.set("n", "<leader>gr", "<cmd>FzfLua lsp_references<CR>", opts) -- Show all references to the symbol under the cursor
+vim.keymap.set("n", "<leader>gt", "<cmd>FzfLua lsp_typedefs<CR>", opts) -- Jump to the type definition of the symbol under the cursor
+vim.keymap.set("n", "<leader>ds", "<cmd>FzfLua lsp_document_symbols<CR>", opts) -- List all symbols (functions, classes, etc.) in the current file
+vim.keymap.set("n", "<leader>ws", "<cmd>FzfLua lsp_workspace_symbols<CR>", opts) -- Search for any symbol across the entire project/workspace
+vim.keymap.set("n", "<leader>gi", "<cmd>FzfLua lsp_implementations<CR>", opts) -- Go to implementation
+
+-- Quarto keymaps
+-- Note: m stands for Meta key; left Alt
+vim.keymap.set({ "n", "i" }, "<m-i>", "<esc>i```{python}<cr>```<esc>O", { desc = "[i]sert Python Code Chunk" }) -- Insert a code chunk for Python in a .qmd file
+vim.keymap.set("n", "<m-t>", ":split term://ipython", { desc = "Split horizontally and open IPython" }) -- Open an IPython instance
