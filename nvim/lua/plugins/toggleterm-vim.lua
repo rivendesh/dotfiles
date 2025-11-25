2c0c90e2fb63883a2697bb0bc413d83ef323787d
Desh Iyer <desh.iyer@gmail.com>
2025-11-25 06:05:34 +0000

    Add ToggleTerm plugin

---
+++ nvim/lua/plugins/toggleterm-vim.lua
@@ -0,0 +1,9 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		close_on_exit = true, -- closes the terminal buffer when the process exits
+		auto_scroll = true,
+		shade_terminals = true,
+	},
+}
